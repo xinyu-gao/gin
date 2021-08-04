5d82f89a448287d9f3397cfff1f90a3762f2a7d0
xinyu <isgaoxy@gmail.com>
2021-08-04 12:16:46 +0800

    整理ä»依赖

--- routers/hello_router.go
+++ routers/hello_router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"gin/api"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func loadUser(e *gin.Engine) {
@@ -10,5 +11,22 @@ func loadUser(e *gin.Engine) {
 	{
 		userGroup.GET("/hello", api.HelloHandler)
 		userGroup.POST("/post", api.PostHandler)
+		userGroup.POST("/login", login)
 	}
+}
+
+type User struct {
+	Username string `json:"username"`
+	Passwd   string `json:"passwd"`
+}
+
+func login(c *gin.Context) {
+	f := c.Param("aaa")
+	json := User{}
+	if err := c.BindJSON(&json); err != nil {
+		return
+	}
+	username := json.Username
+	passwd := json.Passwd
+	c.String(http.StatusOK, "Hello world "+username+"_"+passwd+f)
 }
