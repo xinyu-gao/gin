1e04bca553e3dfdf73370d9a957755f7eb907707
xinyu <isgaoxy@gmail.com>
2021-08-10 17:13:21 +0800

    数据库连接

--- conf/mysql_conf.go
+++ conf/mysql_conf.go
@@ -6,12 +6,12 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var _db *gorm.DB
 
 func init() {
 	sqlConf := GetMysqlConf()
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s",
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s",
 		sqlConf.username,
 		sqlConf.password,
 		sqlConf.host,
@@ -19,15 +19,15 @@ func init() {
 		sqlConf.dbname,
 		sqlConf.timeout)
 	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	_db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("连接数据库失败, error=" + err.Error())
+		panic("连接数据库失败, error = " + err.Error())
 	}
-	sqlDB, _ := db.DB()
+	sqlDB, _ := _db.DB()
 	sqlDB.SetMaxOpenConns(100) //最大连接数
 	sqlDB.SetMaxIdleConns(20)  //最大允许的空闲连接数
 }
 
 func GetDB() *gorm.DB {
-	return db
+	return _db
 }
